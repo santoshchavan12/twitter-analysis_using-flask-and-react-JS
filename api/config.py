311e24a2b9f68851e59b2644890a06201165d202
santoshchavan12 <santhusyc123@gmail.com>
2022-11-07 17:33:00 +0530

    vader added

--- api/config.py
+++ api/config.py
@@ -1,8 +1,5 @@
-api_key = "4hE5yWqzQGzv3WYCujLBmp1ug"
-api_secret_key = "kwRHwfRydwfpxbI3bBRLysEz713flU9YFbhezCx61UyTl5AZV7"
-bearer_token = f"AAAAAAAAAAAAAAAAAAAAAB8PdQEAAAAAW%2F4jQfyI%2F55pVmAji76cpXpPN7g%3DjwNcI3BNydYR0GOBeqUvdm60SVYzBVLlnTxihqT7dz0FhEEloV"
-access_token = "1424226903670628354-eiuIbia2B6CRX5hiMrPX3K56RftrxD"
-access_token_secret = "JDCzVQj8VzaQ49AkIAlAYP1E9PAqj3XEHJKPjGopxalyN"
-user_id = '1424226903670628354'
-# import nltk
-# nltk.download("stopwords")
+api_key = "API_KEY"
+api_secret_key = "API_SECRET_KEY"
+bearer_token = f"BEARER_TOKEN"
+access_token = "ACCESS_TOKEN"
+access_token_secret = "ACCESS_TOKEN_SECRET"
